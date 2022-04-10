636f0d8aefdeb303ae36b7857f22f21bf8f72a4f
Pierre-Louis Serré <plouiserre@gmail.com>
2022-04-10 14:33:34 +0200

    implement log in game.py

--- game.py
+++ game.py
@@ -1,14 +1,10 @@
-from line import Line
-from piece import Piece, ColorCombinaison
-#TODO : à supprimer
 from grid import Grid
 
-#TODO : change class name to board
 class Game: 
-    def __init__(self) :
+    def __init__(self, Log) :
         self.players = ["player1" , "player2"]
+        self.log = Log
     
-    #TODO : manage all game
     def Play(self):
         grid = Grid()
         turn = 1 
@@ -16,15 +12,14 @@ class Game:
         grid.CreateSecretLine()
             
         while turn <= 12 and guesserWins == False:
-            #TODO supprimer print
-            print("\n\nBEGIN Turn number  ", turn)
+            self.log.LogInDebugLevel("BEGIN Turn number  %s" % turn)
             grid.GuessLine(turn)
             #TODO pass by player like createsecretline and guesssecretline or delete player.py
             guesserWins = grid.CorrectLine(turn)
             turn += 1
             
-        print("game finish")
+        self.log.LogInDebugLevel("game finish")
         if guesserWins == True :
-            print("guesser wins")
+            self.log.LogInDebugLevel("guesser wins")
         else :
-            print("master winner")
+            self.log.LogInDebugLevel("master winner")
--- main.py
+++ main.py
@@ -8,7 +8,7 @@ log = Logger(logging.DEBUG, '%(levelname)s: %(asctime)s | %(message)s ', 'utf-8'
 log.LogInDebugLevel("Start games!!!")
 while numberGame <= 20 :
     log.LogInDebugLevel("Partie %s" % numberGame)
-    game = Game()
+    game = Game(log)
     game.Play()
     numberGame += 1
 log.LogInDebugLevel("Game is finish!!!")
