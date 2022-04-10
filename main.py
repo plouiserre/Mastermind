e171673fae2b4e9f3b27a42a6944953f53a8f13e
Pierre-Louis Serré <plouiserre@gmail.com>
2022-04-10 12:05:20 +0200

    first log basic + gitignore

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+import logging
 from game import Game
 
 numberGame = 0 
-while numberGame < 20 : 
+
+logging.basicConfig(format ='%(levelname)s: %(asctime)s | %(message)s ' ,filename='mastermind.log', encoding='utf-8', level=logging.DEBUG)
+logging.warning("Hello world geeks!!!") 
+while numberGame < 20 :
     print("-------------------------------")
     print("Partie ", numberGame)
     game = Game()
