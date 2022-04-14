f8fa1c368d955f6d21ac96297d8c591adfdb2890
Pierre-Louis Serré <plouiserre@gmail.com>
2022-04-14 14:21:14 +0200

    begin render grid

--- game.py
+++ game.py
@@ -5,23 +5,26 @@ class Game:
     def __init__(self, Log) :
         self.players = ["player1" , "player2"]
         self.log = Log
+        self.maxTurn = 12
     
     def Play(self):
         grid = Grid(self.log)
-        render = Render(grid)
+        render = Render(grid, self.maxTurn)
         turn = 1 
         guesserWins = False
         grid.CreateSecretLine()
             
-        while turn <= 12 and guesserWins == False:
+        while turn <= self.maxTurn and guesserWins == False:
             self.log.LogInInfoLevel("BEGIN Turn number  %s" % turn)
             grid.GuessLine(turn)
             #TODO pass by player like createsecretline and guesssecretline or delete player.py
             guesserWins = grid.CorrectLine(turn)
-            render.Rendering(turn)
             turn += 1
             
         self.log.LogInInfoLevel("game finish")
+
+        render.Rendering()
+        
         if guesserWins == True :
             self.log.LogInInfoLevel("guesser wins")
         else :
--- render.py
+++ render.py
@@ -1,7 +1,19 @@
 class Render :
-    def __init__(self, Grid) :
+    def __init__(self, Grid, MaxTurn) :
         self.grid = Grid
+        self.maxTurn = MaxTurn
 
 
-    def Rendering(self, i) :
-        print("Hello World %s" % i)
+    def Rendering(self) :
+        i = 0
+        while i <= self.maxTurn :
+            labelLine =  None 
+            if i == 0 :
+                labelLine = "S" 
+            else :
+                labelLine = i 
+            print(" ---------------------------------- ")
+            print("|%s                                 |" % labelLine)
+            print(" ---------------------------------- ")
+            i += 1
+        
